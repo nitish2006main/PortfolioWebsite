8cb964065616ec01833c21211218b563442294be
Nitish Reddy <nitisht.reddy@gmail.com>
2026-09-24 01:11:19 -0400

    Fixing issue with previous hero section still showin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Navbar from './components/Navbar.jsx'
-import Hero from './components/Hero.jsx'
 import About from './components/About.jsx'
 import Skills from './components/Skills.jsx'
 import Projects from './components/Projects.jsx'
